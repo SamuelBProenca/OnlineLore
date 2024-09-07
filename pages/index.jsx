1ab078b00377f7073e82742fcf7eb92e4e7e04bc
samuel <samuelbrito782@gmail.com>
2024-09-07 14:26:12 -0300

    criação do index.js

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import { HeaderRendering } from "../components/header_component";
+
+function Home(){
+    return <> 
+        <HeaderRendering/>        
+        <h1>That is the amazing headset shop</h1>
+    </>
+}
+
+export default Home;
