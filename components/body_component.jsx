1e748578be3410ddea8785ae4a38a9035e060931
samuel <samuelbrito782@gmail.com>
2024-09-08 15:44:36 -0300

    inclusão dos estilos nos componentes

--- components/body_component.jsx
+++ components/body_component.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import styles from '../styles/card.body.module.css'
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="card">
-      <img src={product.image} alt={product.name} />
-      <h2>{product.name}</h2>
-      <p>{product.price}</p>
-      <p>{product.description}</p>
-      <button onClick={() => alert(`Detalhes do produto: ${product.name}`)}>Ver Detalhes</button>
+    <div className={styles.card_body}>
+      <div className={styles.card}>
+        <img src={product.image} alt={product.name} />
+        <h2>{product.name}</h2>
+        <p>{product.price}</p>
+        <p>{product.description}</p>
+        <button onClick={() => alert(`Detalhes do produto: ${product.name}`)}>Ver Detalhes</button>
+      </div>
     </div>
   );
 };
