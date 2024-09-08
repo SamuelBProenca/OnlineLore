edaf9f9ebe10eac0b8b4791c54d17028d367e020
samuel <samuelbrito782@gmail.com>
2024-09-08 15:45:15 -0300

    index colocado em pages

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,21 @@
-import { HeaderRendering } from "../components/header_component";
+import React from 'react';
+import Product from '../components/assets/assets_item';
+import ProductCard from '../components/body_component';
+import { HeaderRendering } from '../components/header_component';
 
-function Home(){
-    return <> 
-        <HeaderRendering/>        
-        <h1>That is the amazing headset shop</h1>
-    </>
-}
+const products = [
+    new Product(1, 'Produto 1', 'R$ 100,00', '/images/produto1.jpg', 'Descrição do Produto 1'),
+    new Product(2, 'Produto 2', 'R$ 200,00', '/images/produto2.jpg', 'Descrição do Produto 2'),
+];
 
-export default Home;
+const HomePage = () => {
+  return (<>
+    <HeaderRendering />
+      {products.map(product => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+  </>
+  );
+};
+
+export default HomePage;
